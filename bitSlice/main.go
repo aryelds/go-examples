84eb16be585d912a44350646b0b496ea7f96cb25
Aryel Santos <aryel.ds@gmail.com>
2018-12-01 19:40:11 -0200

    bit Slice

---
+++ bitSlice/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	var greeting = "Hello there!"
+
+	fmt.Println([]byte(greeting))
+
+	// result [72 101 108 108 111 32 116 104 101 114 101 33]
+	//http://www.asciitable.com/
+}
