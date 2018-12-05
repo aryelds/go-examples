1059a2e73039092913f847d46a1b2c8fd51277f8
Aryel Santos <aryel.ds@gmail.com>
2018-12-05 10:59:35 -0200

    Map

---
+++ map/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	cor := make(map[string]string)
+	colors := map[string]string {
+		"red": "#ff0000",
+		"green": "#4bf785",
+		"white": "#fffff",
+	}
+
+	fmt.Println(cor)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
+
+
